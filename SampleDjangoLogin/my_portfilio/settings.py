ab7efcfcc79f8c4177602f9a1530012a2c5b25c7
unknown <juliocesarmoralesmena1@gmail.com>
2024-12-02 22:39:04 -0500

    Revert "uh"
    This reverts commit d09a69a8b8fff73be8bdab5d00a9d52471b8a43a.

--- SampleDjangoLogin/my_portfilio/settings.py
+++ SampleDjangoLogin/my_portfilio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,15 +79,10 @@ WSGI_APPLICATION = 'my_portfilio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'stock_db_aul0'),
-        'USER': os.environ.get('DB_USER', 'stock_db_aul0_user'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'ogXHnQTh6IuHGGepsICwFNzpquc02ZaZ'),
-        'HOST': os.environ.get('DB_HOST', 'dpg-ct77jhrtq21c73bk79hg-a.oregon-postgres.render.com'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 TEMPLATES = [
     {
